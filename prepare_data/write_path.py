178e73d3d16c58640e83f943190afcb70adf8d4c
guilk <guiliangke@gmail.com>
2016-08-30 02:15:15 -0700

    add ck+

--- prepare_data/write_path.py
+++ prepare_data/write_path.py
@@ -65,6 +65,29 @@ def prepare_bu_4dfe(src_root, dst_root):
                 results.append(line)
     return results
 
+# CK+
+def prepare_ck(src_root, dst_root):
+    print 'Prepare CK+ dataset'
+    dataset_name = 'CK+'
+    dst_folder = os.path.join(dst_root, dataset_name)
+    if not os.path.exists(dst_folder):
+        os.makedirs(dst_folder)
+    results = []
+    dataset_path = os.path.join(src_root, dataset_name,'extended-cohn-kanade-images/cohn-kanade-images')
+    subjects_folder = os.listdir(dataset_path)
+    for subject_folder in subjects_folder:
+        subject_path = os.path.join(dataset_path, subject_folder)
+        samples = os.listdir(subject_path)
+        for sample in samples:
+            src_path = os.path.join(subject_path, sample)
+            dst_path = os.path.join(dst_folder,subject_folder,sample)
+            line = src_path+','+dst_path+'\n'
+            results.append(line)
+    return results
+
+
+
+
 
 if __name__ == '__main__':
     src_root = '/usr0/multicomp/datasets'
@@ -78,7 +101,10 @@ if __name__ == '__main__':
     # paths = prepare_bu_3dfe(src_root, dst_root)
     # write_path(fw, paths)
 
-    paths = prepare_bu_4dfe(src_root, dst_root)
+    # paths = prepare_bu_4dfe(src_root, dst_root)
+    # write_path(fw, paths)
+
+    paths = prepare_ck(src_root, dst_root)
     write_path(fw, paths)
 
     fw.close()
