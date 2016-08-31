1e094a6f82bd9be85563d60c87ef411fb4fcfc06
guilk <guiliangke@gmail.com>
2016-08-30 18:10:35 -0700

    add analysis

--- prepare_data/analyze_distribution.py
+++ prepare_data/analyze_distribution.py
@@ -2,7 +2,7 @@
 import os
 
 if __name__ == '__main__':
-    data_path = './train.txt'
+    data_path = './test.txt'
     labels = {}
     with open(data_path, 'rb') as fr:
         lines = fr.readlines()
@@ -12,5 +12,4 @@ if __name__ == '__main__':
                 labels[int(splits[1])] = labels[int(splits[1])] + 1
             else:
                 labels[int(splits[1])] = 1
-
     print labels
