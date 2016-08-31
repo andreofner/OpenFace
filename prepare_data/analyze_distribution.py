b8e2b7cdb996aa689dc30fe56250161202b3b6c4
guilk <guiliangke@gmail.com>
2016-08-30 18:01:21 -0700

    add analysis

---
+++ prepare_data/analyze_distribution.py
@@ -0,0 +1,16 @@
+
+import os
+
+if __name__ == '__main__':
+    data_path = './train.txt'
+    labels = {}
+    with open(data_path, 'rb') as fr:
+        lines = fr.readlines()
+        for line in lines:
+            splits = line.split(' ')
+            if int(splits[1]) in labels:
+                labels[int(splits[1])] = labels[int(splits[1])] + 1
+            else:
+                labels[int(splits[1])] = 1
+
+    print labels
